efe2741e2143baa0cda40fa86573164053543d25
Kyle Smith <kylesmith2@gmail.com>
2017-01-20 04:30:58 -0800

    Changes the screenshot script to accept the *.png filepath as an argument.

--- screenshot.py
+++ screenshot.py
@@ -1,12 +1,11 @@
-""" Run this script to save a screenshot of the board """
+""" Run this script to save a png screenshot of the board, the first arg is the file name to save as. """
 from lib import gui
-
-# Save image to the path BOARD_IMAGE_FN
-BOARD_IMAGE_FN = 'board_image.png'
-
+from sys import argv
+from time import sleep
 
 if __name__ == '__main__':
     ''' Get the board image. '''
     gui.find_window()
+    sleep(1)
     board_image = gui.get_board_image()
-    board_image.save(BOARD_IMAGE_FN)
+    board_image.save(argv[1])
